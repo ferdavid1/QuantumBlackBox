15d3fd5c92bfc449afbc4300a9552d632b475d29
ferdavid1 <fespinosa@ucdavis.edu>
2018-01-14 00:15:36 -0800

    small

--- MPU6050_DMP6/MPU6050_DMP6.ino
+++ MPU6050_DMP6/MPU6050_DMP6.ino
@@ -314,12 +314,11 @@ void loop() {
             mpu.dmpGetQuaternion(&q, fifoBuffer);
             mpu.dmpGetGravity(&gravity, &q);
             mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-            Serial.print("ypr\t");
-            Serial.print(ypr[0] * 180/M_PI);
-            Serial.print("\t");
-            Serial.print(ypr[1] * 180/M_PI);
-            Serial.print("\t");
-            Serial.println(ypr[2] * 180/M_PI);
+            //Serial.print("ypr\t");
+            Serial.print(float(ypr[0] * 180/M_PI));
+            Serial.print("\n");
+            //Serial.print(float(ypr[1] * 180/M_PI)), "\n";
+            //Serial.println(float(ypr[2] * 180/M_PI), "\n");
         #endif
 
         #ifdef OUTPUT_READABLE_REALACCEL
