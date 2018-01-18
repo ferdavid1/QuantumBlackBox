dffd850fde65a0df6d121269181034e5c0995864
ferdavid1 <fespinosa@ucdavis.edu>
2018-01-18 11:56:13 -0800

    small

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 import serial
-# import atexit
-import pandas as pd
-import numpy as np
-import sys
-import os
-
-# values = []
-
-cnt=0
 
 serialArduino = serial.Serial('/dev/ttyACM0', 115200) # open serial connection to Arduino
 
 print("serialArduino.isOpen() = " + str(serialArduino.isOpen()))
 
+
 if __name__ == '__main__':
 
-    from turtle import write, pensize, penup, pendown, pencolor, pos, setpos, goto, forward, left, right, backward, mainloop
+    from turtle import write, undo, delay, pensize, penup, pendown, pencolor, pos, setpos, goto, forward, left, right, backward, mainloop
     setpos(0,0)
 
     def dotted_line(hash_length, line_length): # like this --------- at any angle
@@ -30,7 +22,7 @@ if __name__ == '__main__':
 
     def quantum_random_map(angle): # map angle to some corresponding quantum probability amplitude
 
-        
+        return 0
 
     def draw_qubit():
         
@@ -63,20 +55,27 @@ if __name__ == '__main__':
         pencolor("Blue")
 
         write("-------------------------\n\nThe particle's angle from origin is -35.30 degrees.\n\nWhen the gyroscope is aligned at this angle with the particle, likelihood of encountering the particle in that state will read 1.\n\nThe more out of alignment the gyroscope is with the qubit's yaw, the more random the likelihood of that state is.\n\nWhen the gyroscope is orthogonal to the qubit angle of yaw, likelihood of that state is completely random\n\nNote: 'randomness' is evaluated according to a quantum probability amplitude value.\n\n-------------------------")
+        setpos(-200, 100)
         for i in range(4000):
             while (serialArduino.inWaiting()==0):
-                pass
-            #print("reading line...")
+                serialArduino.write(b'a')
+
             try:
                 valueRead = serialArduino.readline(500) #500
-                # print(valueRead)
                 valueInInt = float(valueRead)
-                # print(valueInInt)
+                print(valueInInt)
+                write("Probability: \n")
                 if valueInInt > -36 and valueInInt >= -35 and valueInInt < -34:
-                    print(1)
+                    write(int(1))
+                    undo()
+                    delay(1000)
+
                 else:
                     random_value = quantum_random_map(valueInInt)
-                    print(random_value)
+                    write(random_value)
+                    undo()
+                    delay(1000)
+                    
                     # print("Something else")
                 # values.append(valueInInt) 
             except ValueError:
